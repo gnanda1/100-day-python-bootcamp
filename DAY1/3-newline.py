4e80f17248ab901f7dac204fd11e4307b4def5fc
Ganesh Na <gnanda1987@gmail.com>
2021-06-25 14:33:17 -0700

    Day 1 python scripts.

---
+++ DAY1/3-newline.py
@@ -0,0 +1,3 @@
+# Print string output in new lines.
+
+print("Your \nfuture \ncreated \nby \nwhat \nyou \ndo \ntoday. \n")
---
+++ DAY1/7-input-challenge.py
@@ -0,0 +1,7 @@
+"""
+Write a program that prints the number of characters in a user's name.
+Example Input: Angela
+Example Output:6
+"""
+
+print(len(input("What is your name?")))
